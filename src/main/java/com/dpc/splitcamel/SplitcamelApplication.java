f5d8a108a0ba5673d74f46aed49dd2d9d119ade2
Peter Kovacs <peter.kovacs@dpc.hu>
2019-06-13 14:40:33 +0200

    Initial commit

---
+++ src/main/java/com/dpc/splitcamel/SplitcamelApplication.java
@@ -0,0 +1,13 @@
+package com.dpc.splitcamel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitcamelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplitcamelApplication.class, args);
+    }
+
+}
